fd5e031a3c0ec3dc90a032ce153b22d14515cffb
shaikhashukoor <kkshaikha94@gmail.com>
2026-09-16 12:43:44 +0530

    Create SecondActivity to handle intent data
    Implement SecondActivity to display user data.

---
+++ intent/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.intent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView a1,a2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        a1 = findViewById(R.id.textView);
+        a2 = findViewById(R.id.textView2);
+
+        Bundle extra = getIntent().getExtras();
+        String text = extra.getString("name");
+        String text1 = extra.getString("age");
+        a1.setText(text);
+        a2.setText(text1);
+
+
+    }
+    }
