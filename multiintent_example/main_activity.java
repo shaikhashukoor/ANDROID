8a169f62dba6d7970eb9649292698e89d7944425
shaikhashukoor <kkshaikha94@gmail.com>
2026-09-23 12:47:19 +0530

    Add intent to start SecondActivity with extras
    Implemented button click to start SecondActivity with extras.

---
+++ multiintent_example/main_activity.java
@@ -0,0 +1,30 @@
+ackage com.example.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button);
+    }
+      public void click(View view){
+          Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+          intent.putExtra("name","alen");
+          intent.putExtra("age","25");
+          startActivity(intent);
+
+      }
+    }
